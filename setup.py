5079ffbbb7078d5fee8e0096acb992a10a0666d8
sina <sina.mehrabi1990@gmail.com>
2021-04-25 17:00:09 +0430

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swagger-merger",
-    version="1.0.1",
+    version="1.0.2",
     author="Sina Mehrabi",
     description="swagger merger python package",
     long_description=long_description,
