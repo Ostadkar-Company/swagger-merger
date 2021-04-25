ac2a91201cbffb9f777575a322c5d5765a130096
sina <sina.mehrabi1990@gmail.com>
2021-04-25 16:50:26 +0430

    add setup.py version 1.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swagger-merger",
+    version="1.0.1",
+    author="Sina Mehrabi",
+    description="swagger merger python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url='https://github.com/ostadkarteam/swagger-merger',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["swagger_merger"],
+    package_dir={'':'swagger_merger/src'},
+    install_requires=["deepmerge", "PyYAML"],
+
+)
